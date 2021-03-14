b788fc2a6a1dc41b7cd5d588df5365a2e87b9296
BeterEmad <69284934+Eb03xh@users.noreply.github.com>
2021-03-14 18:58:01 +0200

    Add files via upload

---
+++ ebrees.sh
@@ -0,0 +1,127 @@
+g='\033[1;32m'
+p='\033[1;35m'
+echo -e "$g+++++++++++++++>$p[ebrees el 7akem]$g<++++++++++++++"
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+ sleep 0.5
+ clear
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear
+ echo -e "$g+++++++++++++++>$p[ebrees el 7akem]$g<++++++++++++++"
+pkg install git -y
+pkg install python -y
+pkg install python2 
+pkg install python -y
+pkg install ruby -y
+pkg install php -y
+pkg install bash -y
+pkg install nmap -y
+pkg install clang -y
+pkg install macchanger -y
+pip2 install requests -y
+pip2 install wget -y
+pkg install proot -y
+pkg install w3m -y
+pkg install clang -y
+pkg install net-tools -y
+pkg install zip -y
+pkg install tar -y
+pkg install unzip -y
+pkg install wgetrc -y
+pkg install unzip -y
+pkg install figlet -y
+pkg install curl -y
+gem install lolcat -y
+pkg install help -y
+pkg install toilet -y
+pkg install cowsay -y
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+apt install wireshark -y
+pkg install wget -y
+pkg install figlet -y
+apt install cmatrix -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+apt install wireshark -y
+pkg install unzip -y
+pkg install tor -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wcalc -y
+pkg install openssl -y
+apt-get update -y
+apt install php -y
+pkg install bmon -y
+pkg install fish -y
+pkg install openssh -y
+apt update -y
+apt upgrade -y
+apt-get upgrade -y
+install python -y
+apt install python2 -y
+apt install golang -y
+apt install git -y
+apt install host -y
+apt install nano -y
+apt install havij -y
+apt install hydra -y
+pip install --upgrade pip
+pip install mechanize
+
+
+
+
+
+
+
+
+echo -e "$r+++++++++++++++>$p[ebrees el 7akem]$r<++++++++++++++"
+echo "thanks for using ...!"
+echo "by by [X_X]"
